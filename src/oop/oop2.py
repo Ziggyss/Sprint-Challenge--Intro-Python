abaa8e3d7aae10cd90b916ef86b50ec7878697c4
Lisa Wilton <mishmao14@gmail.com>
2020-02-14 09:09:38 +0000

    tests passing for oop2 and comp

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,10 +4,12 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
-    # TODO
+    def drive(self):
+        return "vroooom"
+
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -16,6 +18,16 @@ class GroundVehicle():
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=2):
+        self.num_wheels = num_wheels
+
+#Come back to this one and check over whether super needs to be added...        
+        
+
+    def drive(self):
+        return "BRAAAP!!"
 
 # TODO
 
@@ -30,3 +42,5 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+for v in vehicles:
+    print(v.drive())
